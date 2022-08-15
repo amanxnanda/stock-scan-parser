74b06005be5cc1729e87c138660e64c45367a5d8
Aman Nanda <amannanda8@gmail.com>
2022-08-16 01:09:55 +0530

    remove home bloc

--- layers/presentation/lib/src/modules/home/bloc/home_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'home_bloc.dart';
-
-/// {@template home_event}
-/// The home event.
-/// {@endtemplate}
-@immutable
-abstract class HomeEvent {}
--- layers/presentation/lib/src/modules/home/view/home_page.dart
+++ layers/presentation/lib/src/modules/home/view/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:stock_scan_presentation/presentation.dart';
+import 'package:stock_scan_domain/domain.dart';
 import 'package:stock_scan_presentation/src/modules/home/view/widgets/widgets.dart';
+import 'package:stock_scan_presentation/src/modules/stocks/bloc/stocks_bloc.dart';
 
 /// {@template home_page}
 /// The home page.
@@ -12,7 +13,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => BlocProvider(
-        create: (context) => HomeBloc(),
+        create: (context) => StocksBloc(
+          stockRepository: context.read<StockRepository>(),
+        ),
         child: const HomeView(),
       );
 }
