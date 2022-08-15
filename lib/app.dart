ff038132ce4c4ace1251d811007831a5723b2858
Aman Nanda <amannanda8@gmail.com>
2022-08-16 00:25:25 +0530

    fix: repository provider

--- lib/app.dart
+++ lib/app.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:stock_scan_data/data.dart';
+import 'package:stock_scan_domain/domain.dart';
 import 'package:stock_scan_presentation/presentation.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => RepositoryProvider(
+  Widget build(BuildContext context) => RepositoryProvider<StockRepository>(
         create: (_) => StockRepositoryImpl(),
         child: ScreenUtilInit(
           builder: (_, __) => MaterialApp.router(
