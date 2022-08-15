f829d273b90d1b1aca9b2950e3d149219a639e9e
Aman Nanda <amannanda8@gmail.com>
2022-08-16 00:14:59 +0530

    wrap with repository provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stock_scan_data/data.dart';
 import 'package:stock_scan_presentation/presentation.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => ScreenUtilInit(
-        builder: (_, __) => MaterialApp.router(
-          routeInformationParser: router.routeInformationParser,
-          routerDelegate: router.routerDelegate,
-          routeInformationProvider: router.routeInformationProvider,
-          theme: kThemeData,
+  Widget build(BuildContext context) => RepositoryProvider(
+        create: (_) => StockRepositoryImpl(),
+        child: ScreenUtilInit(
+          builder: (_, __) => MaterialApp.router(
+            routeInformationParser: router.routeInformationParser,
+            routerDelegate: router.routerDelegate,
+            routeInformationProvider: router.routeInformationProvider,
+            theme: kThemeData,
+            themeMode: ThemeMode.light,
+          ),
         ),
       );
 }
